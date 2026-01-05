52c1d1d87b6f9051e589c140f748e5b37e79f485
Mert Gündoğan <merttgundogan@hotmail.com>
2026-01-05 13:14:31 +0300

    Kayıt olma özelliği eklendi ve klasör yapısı düzenlendi

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from "express";
+import authRoutes from "./routes/authRoutes.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+
+app.use('/api/auth',authRoutes)
+
 app.listen(port,()=>{
     console.log(`Port is on ${port}`);
 })
